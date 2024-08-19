d176d8b61eb01daef005189253848e9d63df2fb2
huzaifa954 <ahmedniazi954@gmail.com>
2024-08-19 11:41:50 -0700

    add new files

---
+++ app.js
@@ -0,0 +1,70 @@
+// let val1=+prompt("enter value1")
+// let operator=prompt("enter operator")
+// let val2=+prompt("enter value2")
+// if(operator === "+" ){ 
+//     alert(val1+val2)
+// }
+//  else if(operator === "*"){
+//     alert(val1*val2)
+//  } else if(operator === "/"){
+//     alert(val1/val2)
+//  }
+//  else if(operator === "-"){
+//     alert(val1-val2)
+//  }
+// else {
+//     alert("enter value")
+// }
+// function cal(array) {
+//     let val1 = +prompt("enter value1")
+//     let operator = prompt("enter operator")
+//     let val2 = +prompt("enter value2")
+//     if (operator === "+") {
+//         alert(val1 + val2)
+//     }
+//     else if (operator === "x") {
+//         alert(val1 * val2)
+//     } else if (operator === "/") {
+//         alert(val1 / val2)
+//     }
+//     else if (operator === "-") {
+//         alert(val1 - val2)
+//     }
+//     else {
+//         alert("enter value")
+//     }
+// }
+// cal()
+// alert(cal)
+// function calc(array) {
+//     let lessthan5=[]
+//     let equalto5=[]
+//     let greaterthan5=[]
+    
+//     for(let i = 0; i < array.length; i++){  
+//         const index = array[i];
+//         console.log(index)
+//         if (index.length < 5)
+//             {
+//                 lessthan5.push(index)
+//             }
+//             else if(index.length === 5){
+//                 equalto5.push(index)
+                
+//             } 
+//             else {
+//                 greaterthan5.push(index)
+                
+//             }
+            
+//         }
+//         console.log(lessthan5)
+//         console.log(equalto5)
+//         console.log(greaterthan5)
+        
+//     }
+//         const x1=["huzaifa","ali","ahmed","mujahid","arif"]
+//         calc(x1)
+let c="huzaifa"
+let d=Array.from(c)
+console.log(d) 
