2ef7f623cd96323761d8896de3c15918cb7ee2f8
huzaifa954 <ahmedniazi954@gmail.com>
2024-08-22 10:19:58 -0700

    add new text

--- app.js
+++ app.js
@@ -1,70 +1,56 @@
-// let val1=+prompt("enter value1")
-// let operator=prompt("enter operator")
-// let val2=+prompt("enter value2")
-// if(operator === "+" ){ 
+// THIS IS CALCULATOR
+// let c="huzaifa"
+// let d=Array.from(c)
+// console.log(d) 
+// let val1=+prompt("enter your first value")
+// let operator=prompt("enter your operator")
+// let val2=+prompt("enter your value2")
+// if (operator === "+") {
 //     alert(val1+val2)
 // }
-//  else if(operator === "*"){
-//     alert(val1*val2)
-//  } else if(operator === "/"){
+// else if(operator === "x"){
+// alert(val1*val1)
+
+// }
+// else if(operator === "/"){
 //     alert(val1/val2)
-//  }
-//  else if(operator === "-"){
+// } 
+// else if (operator === "-"){
 //     alert(val1-val2)
-//  }
+// }
+// else if (operator ==="%")
+//     {
+//         alert(val1%val2)
+//     }
 // else {
-//     alert("enter value")
+//     alert("enter your value ifyou want to calculate")
 // }
-// function cal(array) {
-//     let val1 = +prompt("enter value1")
-//     let operator = prompt("enter operator")
-//     let val2 = +prompt("enter value2")
-//     if (operator === "+") {
-//         alert(val1 + val2)
-//     }
-//     else if (operator === "x") {
-//         alert(val1 * val2)
-//     } else if (operator === "/") {
-//         alert(val1 / val2)
-//     }
-//     else if (operator === "-") {
-//         alert(val1 - val2)
-//     }
-//     else {
-//         alert("enter value")
-//     }
-// }
-// cal()
-// alert(cal)
-// function calc(array) {
-//     let lessthan5=[]
-//     let equalto5=[]
-//     let greaterthan5=[]
-    
-//     for(let i = 0; i < array.length; i++){  
-//         const index = array[i];
-//         console.log(index)
-//         if (index.length < 5)
-//             {
-//                 lessthan5.push(index)
-//             }
-//             else if(index.length === 5){
-//                 equalto5.push(index)
-                
-//             } 
-//             else {
-//                 greaterthan5.push(index)
-                
-//             }
+
+function sub(array) {
+        let greaterthan5=[]
+        let equalto5=[]
+        let lessthan5=[]
+    for (let i = 0; i < array.length; i++) {
+        const inde = array[i];
+        console.log(inde)
+        if (inde.length === 5) {
+            equalto5.push(inde)    
+        } 
+        else if(inde.length < 5){
+            lessthan5.push(inde)
             
-//         }
-//         console.log(lessthan5)
-//         console.log(equalto5)
-//         console.log(greaterthan5)
+        }
+        else {
+            greaterthan5.push(inde)           
+        }
+    }   
+    console.log(equalto5)
+    console.log(lessthan5)
+    console.log(greaterthan5)
         
-//     }
-//         const x1=["huzaifa","ali","ahmed","mujahid","arif"]
-//         calc(x1)
-let c="huzaifa"
-let d=Array.from(c)
-console.log(d) 
+    }
+    const  x1=["huzaifa","ali", "ahmed","subhan","haris","kakoo"]   
+    sub(x1)
+let hur ="Huzaifa"
+let c= Array.from(hur)
+alert(c)
